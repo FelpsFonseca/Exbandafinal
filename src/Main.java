5d54c6311645c898c157a05d1e8c5d6e4c1cac96
Felipe Fonseca Vidal Prado <113259593+FelpsFonseca@users.noreply.github.com>
2025-08-28 21:34:08 -0300

    Mudança de variáveis

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main{
+    public static void main(String args[]){
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Empresario empresario = new Empresario("Manoel Gomes",40028922);
+
+        Membro memb1 = new Membro("Vinicius de Moraes", "Cantor");
+
+        Membro memb2 = new Membro("Dj Azeitona","Samplista");
+
+        Membro memb3 = new Membro("Xin Jinping","Baterista social");
+
+        Musica m1 = new Musica("Tarde em Itapoã", 4.03f);
+
+        Musica m2 = new Musica("I write sins not tragedies", 3.06f);
+
+        Musica m3 = new Musica("Remember the Time", 3.59f);
+
+        Banda banda = new Banda("A Química do Mal","Beatbox");
+        banda.setEmpresario(empresario);
+        banda.addMembro(memb1);
+        banda.addMembro(memb2);
+        banda.addMembro(memb3);
+        banda.addMusica(m1);
+        banda.addMusica(m2);
+        banda.addMusica(m3);
+        banda.mostraInfo();
     }
 }
